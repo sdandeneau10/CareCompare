dbeb13bb8e2487e2e6fa435db465641b866b1040
Jack <jfgerulskis@wpi.edu>
2019-05-11 12:01:20 -0400

    Price compare tool now can display relevant hospital data for hard coded DRGs

--- src/app/medicare-data.service.ts
+++ src/app/medicare-data.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
 import {Hospital} from './Hospital';
 import {Observable, of} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicareDataService {
-  // This class will completely change once backend comes into place, do not want to serve 32mb every time this page is requested
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
-  getData(drgCode: string): Observable<Hospital[]> {
-    const requestData: Array<Hospital> = new Array<Hospital>();
-    for (let i = 0; i < 25; i++) {
-      const cost1 = (Math.abs(Math.random()) * 6000) + 1;
-      const cost2 = (Math.abs(Math.random()) * 6000) + 1;
-      const cost3 = (Math.abs(Math.random()) * 6000) + 1;
-      requestData.push(new Hospital('Hospital ' + i, 'x', cost1, cost2, cost3));
-    }
-
-    return of(requestData);
+  getData(): Observable<any> {
+    return this.http.get('../assets/data/medicare-inpatient-data.json');
   }
 }
--- src/app/price-compare/price-compare.component.ts
+++ src/app/price-compare/price-compare.component.ts
@@ -12,16 +12,33 @@ export class PriceCompareComponent implements OnInit {
   relevantHospitals: Hospital[] = [];
   activeHospital: Hospital;
   procedureName: string;
+  drgCode: string;
+  loading: boolean;
 
   constructor(private dataRequest: MedicareDataService) { }
 
   ngOnInit() {
+    this.loading = true;
+
+    // TODO: allow a user to select this
+    this.drgCode = '001';
     this.requestData('');
   }
 
   requestData(code: string): void {
-    this.dataRequest.getData('').subscribe(arr => {
-      this.relevantHospitals = arr;
+    this.dataRequest.getData().subscribe(data => {
+      for (let i = 0; i < 1000; i++) {
+        if (data[i]['DRG Definition'].indexOf(this.drgCode) >= 0) {
+          const temp = new Hospital(
+            data[i]['Provider Name'],
+            data[i]['Provider Street Address'],
+            data[i]['Average Covered Charges'],
+            data[i]['Average Total Payments'],
+            data[i]['Average Medicare Payments']);
+          this.relevantHospitals.push(temp);
+        }
+      }
+      this.loading = false;
     });
   }
 
