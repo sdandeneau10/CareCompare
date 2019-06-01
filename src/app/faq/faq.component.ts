ff42abfcf4991ade5f5ad3063dad656b4b9200a8
Jack Gerulskis <jfgerulskis@wpi.edu>
2019-06-01 13:06:34 -0400

    ye

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FaqService} from "../faq.service";
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FAQComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private questionsService: FaqService) { }
+  drgQandA: string[][];
   ngOnInit() {
+    this.drgQandA = this.getQuestionsAndAnswers('drg-codes.json');
+  }
+
+  getQuestionsAndAnswers(url: string): string[][] {
+    console.log('fire!');
+    const data: string[][] = [];
+    this.questionsService.getQuestions(url).subscribe(results => {
+      console.log(results);
+      for (const entry in results) {
+        console.log(results[entry]);
+        data.push(results[entry]);
+      }
+      this.loaded1 = true;
+    });
+    console.log('loaded!');
+    return data;
   }
 
 }
