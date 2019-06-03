8ded47cb42398670c2cf69c34aa62630e36f7367
Jack Gerulskis <jfgerulskis@wpi.edu>
2019-06-02 20:37:47 -0400

    FAQs update

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FaqService} from "../faq.service";
+import {FaqService} from '../faq.service';
+import { MEDICARE_DRG_CODES } from '../medicareConstants';
+
 
 @Component({
   selector: 'app-faq',
@@ -10,12 +12,18 @@ export class FAQComponent implements OnInit {
 
   constructor(private questionsService: FaqService) { }
   drgQandA: string[][];
+  ourDataQandA: string[][];
+  targetID: number;
+  drgCodes: string[];
+
   ngOnInit() {
     this.drgQandA = this.getQuestionsAndAnswers('drg-codes.json');
+    this.ourDataQandA = this.getQuestionsAndAnswers('our-data.json');
+    this.targetID = 0;
+    this.drgCodes = MEDICARE_DRG_CODES;
   }
 
   getQuestionsAndAnswers(url: string): string[][] {
-    console.log('fire!');
     const data: string[][] = [];
     this.questionsService.getQuestions(url).subscribe(results => {
       console.log(results);
@@ -23,10 +31,17 @@ export class FAQComponent implements OnInit {
         console.log(results[entry]);
         data.push(results[entry]);
       }
-      this.loaded1 = true;
     });
-    console.log('loaded!');
     return data;
+  }
+
+  getNextTargetID(): string {
+    this.targetID++;
+    return '#target' + this.targetID;
+  }
+
+  getCurrentTargetID(): string {
+    return 'target' + this.targetID;
   }
 
 }
