36fff6e9adccad83cacb782d19cecafda5b577a3
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-06-06 16:01:18 -0400

    More work on drg table page

--- src/app/drgtable/drgtable.component.ts
+++ src/app/drgtable/drgtable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MEDICARE_DRG_CODES } from '../medicareConstants';
 
 @Component({
   selector: 'app-drgtable',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drgtable.component.css']
 })
 export class DrgtableComponent implements OnInit {
+  drgCodes: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.drgCodes = MEDICARE_DRG_CODES;
   }
 
 }
