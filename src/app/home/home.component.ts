ae8ad10d6ed70bb9c0c4b831a262229e38961680
Connor Burri <cjburri@wpi.edu>
2019-06-26 03:01:09 -0400

    Homepage Progress! toyed with some animations, and changed the navbar a little to make it look cleaner with the interface I had in mind. Note: this is nowhere near done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
+import { delay } from 'q';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger("fade-in", [
+      transition("void => *", [
+        style({opacity: 0}),
+        animate("2000ms")
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+
+  title: boolean;
+  video: boolean;
+
+  phrase1: boolean;
+  phrase2: boolean;
+  phrase3: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.animationSequence();
+  }
+
+  async animationSequence(){
+    this.title = true;
+    this.video = false;
+    this.phrase1 = false;
+    this.phrase2 = false;
+    this.phrase3 = false;
+    await this.delay(1000);
+    this.phrase1 = true;
+    await this.delay(333);
+    this.phrase2 = true;
+    await this.delay(333);
+    this.phrase3 = true;
+    await this.delay(1000);
+    this.video = true;
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
 }
