c8c322d44d6ab97b29b117f305ea88fe805a69b2
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-07-11 20:57:02 -0400

    DRG page fixes

--- src/app/drgtable/drgtable.component.ts
+++ src/app/drgtable/drgtable.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MEDICARE_DRG_CODES } from '../medicareConstants';
+import {MedicareDataService} from '../medicare-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-drgtable',
@@ -10,13 +12,28 @@ export class DrgtableComponent implements OnInit {
   fulldrgCodes: string[];
   p: number;
   nope: string;
+  codes: string[];
+  s = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.p = 1;
     this.nope = 'This drg does not exist!';
     this.fulldrgCodes = MEDICARE_DRG_CODES;
+    this.codes = this.fulldrgCodes;
+  }
+  update(s: string) {
+    this.codes = [];
+    for (const code of this.fulldrgCodes) {
+      if (code.includes(s.toUpperCase())) {
+        this.codes.push(code);
+      }
+    }
+  }
+  compare(s: string) {
+    MedicareDataService.selectedDRG = s;
+    this.router.navigate(['/', 'priceCompare'])
   }
 }
 
