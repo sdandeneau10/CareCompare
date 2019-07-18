77f0530e701b52ed7f42ddb75b86ff8950cf1c07
mughees605 <mughees605@gmail.com>
2019-07-18 23:08:40 +0500

    Successfully rendered components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './auth/auth.service';
+import Auth from '@aws-amplify/auth';
+import Storage from '@aws-amplify/storage';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CareCompare';
+  avatar: string;
+
+  constructor(
+    public auth: AuthService,
+  ) {
+    auth.authState.subscribe((event: string) => {
+      debugger
+      if (event === AuthService.SIGN_IN)
+        this.checkSession();
+      if (event === AuthService.SIGN_OUT)
+        this.avatar = undefined;
+    });
+  }
+
+  ngOnInit() {
+    this.checkSession();
+  }
+
+  async checkSession() {
+    try {
+      const userInfo = await Auth.currentUserInfo();
+      if (userInfo && userInfo.attributes.profile) {
+        const avatar = userInfo.attributes.profile;
+        const url = await Storage.vault.get(avatar) as string;
+        this.avatar = url;
+      }
+    } catch (error) {
+      console.log('no session: ', error);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PriceCompareComponent } from './price-compare/price-compare.component';
 import { FAQComponent } from './faq/faq.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+// import { SignUpComponent } from './sign-up/sign-up.component';
 import { AccountComponent } from './account/account.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgmCoreModule } from '@agm/core';
@@ -18,6 +18,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HospitalPopupComponent } from './hospital-popup/hospital-popup.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
+import { AuthService } from './auth/auth.service';
+import { MaterialModule } from './material/material.module';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { ConfirmCodeComponent } from './auth/confirm-code/confirm-code.component';
+import { LoaderComponent } from './loader/loader.component';
+import { ResetPComponent } from './auth/reset-password/reset-password.component'
+import { ProfileComponent } from './auth/profile/profile.component';
+import { AuthComponent } from './auth/auth.component';
+
+
 
 @NgModule({
   declarations: [
@@ -32,6 +43,12 @@ import { MaintenancePageComponent } from './maintenance-page/maintenance-page.co
     HospitalPopupComponent,
     ContactUsComponent,
     MaintenancePageComponent,
+    SignInComponent,
+    ConfirmCodeComponent,
+    LoaderComponent,
+    ResetPComponent,
+    ProfileComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
@@ -40,13 +57,14 @@ import { MaintenancePageComponent } from './maintenance-page/maintenance-page.co
     FormsModule,
     ReactiveFormsModule,
     AutocompleteLibModule,
+    MaterialModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAwzRGaPm9KP5ZjKvNs5qhFs3p0wePaI4c'
     }),
     BrowserAnimationsModule,
     NgxPaginationModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
