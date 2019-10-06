e6f3403c70ef559b23ec1fe1e7593d8980e00aa7
Ken Desrosiers <kdesrosiers@wpi.edu>
2019-10-06 17:42:11 -0400

    finished contact page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  sendEmail() {
+    const name = (<HTMLInputElement>document.querySelector('#name')).value
+    const email = (<HTMLInputElement>document.querySelector('#email')).value
+    const subject = (<HTMLInputElement>document.querySelector('#subject')).value
+    const message = (<HTMLInputElement>document.querySelector('#message')).value
+    this.http.post('https://l2267an2y8.execute-api.us-east-1.amazonaws.com/sendEmail', {
+      "name": name,
+      "email": email,
+      "subject": subject,
+      "message": message
+    })
+    .subscribe(
+      data => {
+        console.log("POST Request is successful", data);
+      },
+      error => {
+        console.log("Error", error);
+      }
+    );
   }
 
 }
